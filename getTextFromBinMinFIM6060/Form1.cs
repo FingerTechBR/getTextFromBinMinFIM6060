d1c9c71ac4d79726e72e708a63f0ea83567bb786
LincolnBerlick <lincoln.berlick@gmail.com>
2019-08-22 12:32:35 -0300

    subindo arquivos

---
+++ getTextFromBinMinFIM6060/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace getTextFromBinMinFIM6060
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        String filename;
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                filename = openFileDialog.FileName;
+                pathfilename.Text = filename;
+              
+                //MessageBox.Show(filename);
+
+            }
+
+
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+    }
+}
